56e9116836e62c29d7009a34439387c3e9bda7ef
m0000amir <mukhtarov.amir.a@gmail.com>
2024-08-11 23:47:27 +0300

    1st commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+// main.cpp
+#include "raylib.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Start!" << endl;
+    InitWindow(750, 750, "Snake Retro");
+
+    CloseWindow();
+    // // Initialization
+    // const int screenWidth = 800;
+    // const int screenHeight = 600;
+    // InitWindow(screenWidth, screenHeight, "Raylib 5.0 - C++ Template");
+
+    // // Main game loop
+    // while (!WindowShouldClose()) { 
+    //     // Update
+
+    //     // Draw
+    //     BeginDrawing();
+    //     ClearBackground(RAYWHITE);
+    //     DrawText("AAAAAAAWelcome to Raylib 5.0!`", 190, 200, 20, LIGHTGRAY);
+    //     EndDrawing();
+    // }
+
+    // // De-Initialization
+    // CloseWindow(); 
+
+    // return 0;
+
+    return 0;
+}
